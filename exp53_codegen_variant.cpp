9b9c973d62bc4a2863ccaff3e7866ac3874715fc
prakharraj-numerics <157787880+prakharraj-numerics@users.noreply.github.com>
2026-09-01 03:57:13 +0530

    Add compiler codegen variant wrapper

---
+++ exp53_codegen_variant.cpp
@@ -0,0 +1,11 @@
+#ifndef EXP53_VARIANT_FN
+#error EXP53_VARIANT_FN required
+#endif
+#ifndef EXP53_TAIL_FN
+#error EXP53_TAIL_FN required
+#endif
+#define exp53_n2_vmstyle_u4_0381_frozen EXP53_VARIANT_FN
+#define exp53_n2_fused_u4_038_frozen EXP53_TAIL_FN
+#include "exp53_n2_vmstyle_u4_0381_frozen.c"
+#undef exp53_n2_fused_u4_038_frozen
+#undef exp53_n2_vmstyle_u4_0381_frozen
